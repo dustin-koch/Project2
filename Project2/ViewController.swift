7b2af38725632fe2044e7fe3bee9a74fbad6c622
Dustin Koch <dustin.koch@me.com>
2020-02-18 11:48:05 -0500

    initial setup

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    var countries: [String] = []
+    var score = 0
+    
+    // MARK: - Outlets
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    
+    
+    // MARK: - Lifecycle functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCountries()
+        setupUI()
+        askQuestion()
+    }
+    
+    // MARK: - CRUD functions
+    func askQuestion() {
+        buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
+        buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
+        buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
-}
+} // END OF CLASS
 
+// MARK: - Helper function extensions
+extension ViewController {
+    
+    func setupCountries() {
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    func setupUI() {
+        buttonOne.layer.borderWidth = 1
+        buttonTwo.layer.borderWidth = 1
+        buttonThree.layer.borderWidth = 1
+
+        buttonOne.layer.borderColor = UIColor.lightGray.cgColor
+        buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
+        buttonThree.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+}
